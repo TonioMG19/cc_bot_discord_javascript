91affecbd7ed8a2703a90c3fa33bd0af7ecaa9e4
TonioMG19 <toniokuhn2@gmail.com>
2020-04-20 16:47:02 +0200

    tset

--- Bot.js
+++ Bot.js
@@ -1,8 +1,23 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
+const prefix = '!'
+
 bot.on("ready",function(){
     console.log("Carapuce is in the place !");
 });
+
+bot.on("message",message => {
+    switch(message.content){
+        case prefix+"ping":
+            message.channel.send("!Carapong");
+            break;
+        case prefix+"carabonjour":
+            message.reply("Carabonjour à toi !");
+            message.react("😃");
+            break;
+    }
+});
+
 
 bot.login("token")
